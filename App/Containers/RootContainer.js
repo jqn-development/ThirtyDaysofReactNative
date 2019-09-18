3236e9bcbaca65bbac517920da561856a7330bc5
jQN <jqn@jQNs-MacBook-Pro.local>
2019-09-18 14:57:41 +0200

    initial commit with storybook integration

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,34 +1,63 @@
-import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
-import ReduxNavigation from '../Navigation/ReduxNavigation'
-import { connect } from 'react-redux'
-import StartupActions from '../Redux/StartupRedux'
-import ReduxPersist from '../Config/ReduxPersist'
+import React, { Component } from "react";
+import { View, StatusBar } from "react-native";
+import ReduxNavigation from "../Navigation/ReduxNavigation";
+import { connect } from "react-redux";
+import StartupActions from "../Redux/StartupRedux";
+import ReduxPersist from "../Config/ReduxPersist";
+import { getStorybookUI, configure } from "@storybook/react-native";
 
 // Styles
-import styles from './Styles/RootContainerStyles'
+import styles from "./Styles/RootContainerStyles";
 
 class RootContainer extends Component {
-  componentDidMount () {
+  componentDidMount() {
     // if redux persist is not active fire startup action
     if (!ReduxPersist.active) {
-      this.props.startup()
+      this.props.startup();
     }
   }
 
-  render () {
+  render() {
     return (
       <View style={styles.applicationView}>
-        <StatusBar barStyle='light-content' />
+        <StatusBar barStyle="light-content" />
         <ReduxNavigation />
       </View>
-    )
+    );
   }
 }
 
 // wraps dispatch to create nicer functions to call within our component
-const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup())
-})
+const mapDispatchToProps = dispatch => ({
+  startup: () => dispatch(StartupActions.startup()),
+});
 
-export default connect(null, mapDispatchToProps)(RootContainer)
+// export default connect(
+//   null,
+//   mapDispatchToProps,
+// )(RootContainer);
+
+/**
+ * Setup Storybook. It would probably be a fantastic idea to not do this when not in dev.
+ * That is an exercise for the consumer though
+ */
+configure(() => {
+  require("../Components/Stories");
+}, module);
+
+const StorybookUIRoot = getStorybookUI({ port: 7007, onDeviceUI: true });
+
+class StorybookUIHMRRoot extends Component {
+  render() {
+    return <StorybookUIRoot />;
+  }
+}
+
+export default console.tron.storybookSwitcher(StorybookUIHMRRoot)(
+  console.tron.overlay(
+    connect(
+      null,
+      mapDispatchToProps,
+    )(RootContainer),
+  ),
+);
--- storybook/storybook.js
+++ storybook/storybook.js
@@ -1,13 +1,22 @@
-import { AppRegistry } from 'react-native'
-import { getStorybookUI, configure } from '@storybook/react-native'
+import { AppRegistry } from "react-native";
+import { getStorybookUI, configure } from "@storybook/react-native";
 
 // import stories
 configure(() => {
-  require('../App/Components/Stories')
-}, module)
+  require("../App/Components/Stories");
+}, module);
 
 // This assumes that storybook is running on the same host as your RN packager,
 // to set manually use, e.g. host: 'localhost' option
-const StorybookUI = getStorybookUI({ port: 7007, onDeviceUI: true })
-AppRegistry.registerComponent('ThirtyDaysofReactNative', () => StorybookUI)
-export default StorybookUI
+const StorybookUI = getStorybookUI({ port: 7007, onDeviceUI: true });
+class StorybookUIHMRRoot extends Component {
+  render() {
+    return <StorybookUI />;
+  }
+}
+AppRegistry.registerComponent(
+  "ThirtyDaysofReactNative",
+  () => StorybookUIHMRRoot,
+);
+
+export default StorybookUI;
